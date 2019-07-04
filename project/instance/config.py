044dbe4843637a698d0526cd17eddd17c7389dff
Big Lebowski <gmar@image.ece.ntua.gr>
2019-07-04 05:50:57 +0200

    Add schema for data validation before storing

--- project/instance/config.py
+++ project/instance/config.py
@@ -5,3 +5,21 @@ MONGO_DBNAME = 'data-classification'
 MQTT_BROKER = 'mqtt'
 MQTT_PORT = 1883
 MQTT_KEEPALIVE = 60
+
+classification_schema = {
+    "type": "object",
+    "properties": {
+        "status": {"type": "string"},
+        "imagePath": {"type": "string"},
+        "imageId": {"type": "string"},
+        "output": {
+            "type": "object",
+            "properties": {
+                "probability": {"type": "number"},
+                "label": {"type": "string"},
+                "result": {"type": "string"},
+                "bbox": {"type": "array"}
+            }
+        }
+    }
+}
